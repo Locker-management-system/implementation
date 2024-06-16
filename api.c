042909b44aa232ad3416c43771c9411fdfa38046
Kim5ungkwang <ksg6736@jbnu.ac.kr>
2024-06-17 02:25:08 +0900

    api소스코드 getch()추

---
+++ api.c
@@ -0,0 +1,24 @@
+#include <termio.h>
+#include <stdio.h>
+
+int getch(void) {
+    int ch;
+
+    struct termios buf;
+    struct termios save;
+
+    tcgetattr(0, &save);
+    buf = save;
+
+    buf.c_cflag &= ~(ICANON|ECHO);
+    buf.c_cc[VMIN] = 1;
+    buf.c_cc[VTIME] = 0;
+
+    tcsetattr(0, TCSAFLUSH, &buf);
+
+    ch = getchar();
+
+    tcsetattr(0, TCSAFLUSH, &save);
+
+    return ch;
+}
---
+++ api.h
@@ -0,0 +1,10 @@
+//
+// Created by sungkwang on 24. 6. 17.
+//
+
+#ifndef API_H
+#define API_H
+
+int getch(void);
+
+#endif //API_H
